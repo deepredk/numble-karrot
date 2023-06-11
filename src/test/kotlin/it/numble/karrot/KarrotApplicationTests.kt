b2d8fe7bf9ae706a9df55552f5abe9e46d796443
Jinhong Kim <deepred.kim@gmail.com>
2023-06-11 17:16:58 +0900

    Initial commit: 개발환경 구성

---
+++ src/test/kotlin/it/numble/karrot/KarrotApplicationTests.kt
@@ -0,0 +1,13 @@
+package it.numble.karrot
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KarrotApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
